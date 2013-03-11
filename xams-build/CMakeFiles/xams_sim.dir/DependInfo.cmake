f3de58a77f29ea1f5a26e41d04d2e4fcf372a7df
rolfs <rolfs@nikhef.nl>
2013-03-11 11:52:49 +0100

    Added xams-build and updated README with new directory names

---
+++ xams-build/CMakeFiles/xams_sim.dir/DependInfo.cmake
@@ -0,0 +1,55 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/caballito/simulation/geant4/xams/xams-code/src/XAMSAnalysisManager.cc" "/home/caballito/simulation/geant4/xams/xams-build/CMakeFiles/xams_sim.dir/src/XAMSAnalysisManager.cc.o"
+  "/home/caballito/simulation/geant4/xams/xams-code/src/XAMSDetectorConstruction.cc" "/home/caballito/simulation/geant4/xams/xams-build/CMakeFiles/xams_sim.dir/src/XAMSDetectorConstruction.cc.o"
+  "/home/caballito/simulation/geant4/xams/xams-code/src/XAMSDetectorMessenger.cc" "/home/caballito/simulation/geant4/xams/xams-build/CMakeFiles/xams_sim.dir/src/XAMSDetectorMessenger.cc.o"
+  "/home/caballito/simulation/geant4/xams/xams-code/src/XAMSEventAction.cc" "/home/caballito/simulation/geant4/xams/xams-build/CMakeFiles/xams_sim.dir/src/XAMSEventAction.cc.o"
+  "/home/caballito/simulation/geant4/xams/xams-code/src/XAMSEventData.cc" "/home/caballito/simulation/geant4/xams/xams-build/CMakeFiles/xams_sim.dir/src/XAMSEventData.cc.o"
+  "/home/caballito/simulation/geant4/xams/xams-code/src/XAMSHit.cc" "/home/caballito/simulation/geant4/xams/xams-build/CMakeFiles/xams_sim.dir/src/XAMSHit.cc.o"
+  "/home/caballito/simulation/geant4/xams/xams-code/src/XAMSPhysicsList.cc" "/home/caballito/simulation/geant4/xams/xams-build/CMakeFiles/xams_sim.dir/src/XAMSPhysicsList.cc.o"
+  "/home/caballito/simulation/geant4/xams/xams-code/src/XAMSPhysicsMessenger.cc" "/home/caballito/simulation/geant4/xams/xams-build/CMakeFiles/xams_sim.dir/src/XAMSPhysicsMessenger.cc.o"
+  "/home/caballito/simulation/geant4/xams/xams-code/src/XAMSPrimaryGeneratorAction.cc" "/home/caballito/simulation/geant4/xams/xams-build/CMakeFiles/xams_sim.dir/src/XAMSPrimaryGeneratorAction.cc.o"
+  "/home/caballito/simulation/geant4/xams/xams-code/src/XAMSPrimaryGeneratorMessenger.cc" "/home/caballito/simulation/geant4/xams/xams-build/CMakeFiles/xams_sim.dir/src/XAMSPrimaryGeneratorMessenger.cc.o"
+  "/home/caballito/simulation/geant4/xams/xams-code/src/XAMSRunAction.cc" "/home/caballito/simulation/geant4/xams/xams-build/CMakeFiles/xams_sim.dir/src/XAMSRunAction.cc.o"
+  "/home/caballito/simulation/geant4/xams/xams-code/src/XAMSRunActionMessenger.cc" "/home/caballito/simulation/geant4/xams/xams-build/CMakeFiles/xams_sim.dir/src/XAMSRunActionMessenger.cc.o"
+  "/home/caballito/simulation/geant4/xams/xams-code/src/XAMSSensitiveDetector.cc" "/home/caballito/simulation/geant4/xams/xams-build/CMakeFiles/xams_sim.dir/src/XAMSSensitiveDetector.cc.o"
+  "/home/caballito/simulation/geant4/xams/xams-code/src/XAMSStackingAction.cc" "/home/caballito/simulation/geant4/xams/xams-build/CMakeFiles/xams_sim.dir/src/XAMSStackingAction.cc.o"
+  "/home/caballito/simulation/geant4/xams/xams-code/src/fileMerger.cc" "/home/caballito/simulation/geant4/xams/xams-build/CMakeFiles/xams_sim.dir/src/fileMerger.cc.o"
+  "/home/caballito/simulation/geant4/xams/xams-code/xams_sim.cc" "/home/caballito/simulation/geant4/xams/xams-build/CMakeFiles/xams_sim.dir/xams_sim.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "G4_STORE_TRAJECTORY"
+  "G4VERBOSE"
+  "G4UI_USE"
+  "G4VIS_USE"
+  "G4UI_USE_TCSH"
+  "G4INTY_USE_QT"
+  "G4UI_USE_QT"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGL"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/caballito/simulation/geant4/geant4.9.5.p01-install/include/Geant4"
+  "/usr/include/qt4"
+  "/usr/include/qt4/QtCore"
+  "/usr/include/qt4/QtGui"
+  "/usr/include/qt4/QtOpenGL"
+  "/home/caballito/simulation/geant4/xams/xams-code/include"
+  "/home/caballito/downloads/progs/root/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ xams-build/CMakeFiles/xams_sim.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/xams_sim.dir/xams_sim.cc.o"
+  "CMakeFiles/xams_sim.dir/src/XAMSSensitiveDetector.cc.o"
+  "CMakeFiles/xams_sim.dir/src/XAMSDetectorConstruction.cc.o"
+  "CMakeFiles/xams_sim.dir/src/XAMSEventAction.cc.o"
+  "CMakeFiles/xams_sim.dir/src/XAMSPhysicsList.cc.o"
+  "CMakeFiles/xams_sim.dir/src/XAMSPhysicsMessenger.cc.o"
+  "CMakeFiles/xams_sim.dir/src/XAMSRunAction.cc.o"
+  "CMakeFiles/xams_sim.dir/src/fileMerger.cc.o"
+  "CMakeFiles/xams_sim.dir/src/XAMSAnalysisManager.cc.o"
+  "CMakeFiles/xams_sim.dir/src/XAMSHit.cc.o"
+  "CMakeFiles/xams_sim.dir/src/XAMSStackingAction.cc.o"
+  "CMakeFiles/xams_sim.dir/src/XAMSPrimaryGeneratorMessenger.cc.o"
+  "CMakeFiles/xams_sim.dir/src/XAMSPrimaryGeneratorAction.cc.o"
+  "CMakeFiles/xams_sim.dir/src/XAMSRunActionMessenger.cc.o"
+  "CMakeFiles/xams_sim.dir/src/XAMSDetectorMessenger.cc.o"
+  "CMakeFiles/xams_sim.dir/src/XAMSEventData.cc.o"
+  "xams_sim.pdb"
+  "xams_sim"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/xams_sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
