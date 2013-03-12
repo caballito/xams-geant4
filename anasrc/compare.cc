9be048ae25f8ac55221a7da94007fcdc5836a005
rolfs <rolfs@nikhef.nl>
2013-03-12 15:50:44 +0100

    Extend compare.cc for energy deposition processes

--- anasrc/compare.cc
+++ anasrc/compare.cc
@@ -20,18 +20,36 @@
 #include <sstream>
 using namespace std ;
 
-int makeSteplengthHists(const int, Int_t, TH1F*[], TTree*[], TLegend*) ;
+int makeSteplengthHists(const int, Int_t, TH1F*[], TTree*[], TLegend*[]) ;
+int makeEdprocHists(const int, Int_t, TH1D***, TTree*[], TLegend*[]) ;
 
 int compare(Int_t nbrFiles, TString fName[]){
+	// Navigation to the trees.
 	const int nFiles = nbrFiles ;
 	TFile* f[nFiles] ;
 	TDirectoryFile* dir[nFiles] ;
 	TTree* tree[nFiles] ;
-	//
+	// Histograms for the parameters to compare.
 	TH1F* steplength[nFiles] ;
-	TLegend* leg = new TLegend(0.7,0.6,0.9,0.9) ;
-	leg->SetFillColor(0) ;
-	
+	TH1D*** depoproc ;
+	depoproc = new TH1D**[nFiles] ;	// For e- and gamma.
+	for (Int_t i=0; i<nFiles; i++)
+		depoproc[i] = new TH1D*[2] ;
+	//
+	TLegend* leg[3] ;	// One for every histogram.
+	leg[0] = new TLegend(0.7,0.6,0.9,0.9) ;
+	leg[0]->SetFillColor(0) ;
+	leg[1] = new TLegend(0.7,0.6,0.9,0.9) ;
+	leg[1]->SetFillColor(0) ;
+	leg[2] = new TLegend(0.7,0.6,0.9,0.9) ;
+	leg[2]->SetFillColor(0) ;
+
+	// For later use.
+	TString outName("output.root") ;
+	TFile out(outName,"recreate") ;
+	TCanvas can1("can1") ;
+	can1.cd() ;
+
 	for (Int_t i=0; i<nFiles; i++){
 		// Read in the files.
 		f[i] = new TFile(fName[i]) ;
@@ -45,23 +63,46 @@ int compare(Int_t nbrFiles, TString fName[]){
 		dir[i]->cd() ;
 		tree[i] = dynamic_cast<TTree*>(dir[i]->Get("events")) ;
 		
-		if ( makeSteplengthHists(nFiles,i,steplength,tree,leg) )
-			cout << "EE Something's wrong with making the step length histograms."
+		if ( makeSteplengthHists(nFiles, i, steplength, tree, leg) )
+			cout << "EE Something went wrong with the step length histograms."
 				<< endl ;
+
+		if ( makeEdprocHists(nFiles, i, depoproc, tree, leg) )
+			cout << "EE Something went wrong with the energy deposition process histograms." << endl ;
 	}
 
 	steplength[0]->Draw() ;
 	for (Int_t i=1; i<nFiles; i++)
 		steplength[i]->Draw("same") ;
-	leg->Draw("same") ;
+	leg[0]->Draw("same") ;
+	can1.Update() ;
+	//can1.WaitPrimitive() ;
+	//
+	TCanvas can2("can2") ;
+	can2.Divide(1,2) ;
+	for (Int_t j=0;j<2;j++){
+		can2.cd(j+1) ;
+		depoproc[0][j]->Draw() ;
+		for (Int_t i=1; i<nFiles; i++)
+			depoproc[i][j]->Draw("same") ;
+		leg[j+1]->Draw("same") ;
+	}
+	can2.Update() ;
+	can2.WaitPrimitive() ;
 
-	//delete f ; delete dir ; delete tree ; delete steplength ;
+	out.cd() ;
+	can1.Write() ;
+	can2.Write() ;
+	cout << "Histograms written into " << outName << endl ;
+	out.Close() ;
 
+	can1.WaitPrimitive() ;
+	can2.WaitPrimitive() ;
 	return 0 ;
 }
 
 int makeSteplengthHists(const int nFiles, Int_t i, TH1F* steplength[],
-		TTree* tree[], TLegend* leg){
+		TTree* tree[], TLegend* leg[]){
 	stringstream dummyName[nFiles] ;
 	stringstream dummyTarget[nFiles] ;
 	TString histName[nFiles] ;
@@ -75,13 +116,50 @@ int makeSteplengthHists(const int nFiles, Int_t i, TH1F* steplength[],
 
 	// Create and draw histograms.
 	steplength[i] = new TH1F(histName[i],
-		"step length;step length (mm);entries",500,0,500) ;
+		"step length of gammas;step length (mm);entries",500,0,500) ;
 	tree[i]->Draw(drawTarget[i],"type.data()==\"gamma\" ") ;
 	cout << "++ Drawing " << histName[i] << " with " 
 		<< steplength[i]->GetEntries() << " entries." << endl ;
 
 	steplength[i]->SetFillColor(19-i) ;
-	leg->AddEntry(steplength[i],histName[i],"f") ;
+	leg[0]->AddEntry(steplength[i],histName[i],"f") ;
+
+	return 0 ;
+}
+
+int makeEdprocHists(const int nFiles, Int_t i, TH1D*** depoproc,
+		TTree* tree[], TLegend* leg[]){
+	stringstream dummyName[nFiles][2] ;
+	stringstream dummyTarget[nFiles][2] ;
+	TString histName[nFiles][2] ;
+	TString drawTarget[nFiles][2] ;
+	
+	// Create name strings.
+	for (Int_t j=0;j<2;j++){
+		dummyName[i][j] << "depoproc[" << i << "][" << j << "]" ;
+		histName[i][j] = (dummyName[i][j].str()).c_str() ;
+		dummyTarget[i][j] << "edproc>>" << histName[i][j] ;
+		drawTarget[i][j] = (dummyTarget[i][j].str()).c_str() ;
+	}
+
+	// Create and draw histograms.
+	// For gammas.
+	depoproc[i][0] = new TH1D(histName[i][0],
+		"gamma;energy deposition process;entries",5,0,5) ;
+	tree[i]->Draw(drawTarget[i][0],"type.data()==\"gamma\" ") ;
+	cout << "++ Drawing " << histName[i][0] << " with " 
+		<< depoproc[i][0]->GetEntries() << " entries." << endl ;
+	// For electrons.
+	depoproc[i][1] = new TH1D(histName[i][1],
+		"electron;energy deposition process;entries",5,0,5) ;
+	tree[i]->Draw(drawTarget[i][1],"type.data()==\"e-\" ") ;
+	//cout << "++ Drawing " << histName[i] << " with " 
+	//	<< steplength[i]->GetEntries() << " entries." << endl ;
+
+	depoproc[i][0]->SetFillColor(19-i) ;
+	leg[1]->AddEntry(depoproc[i][0],histName[i][0],"f") ;
+	depoproc[i][1]->SetFillColor(19-i) ;
+	leg[2]->AddEntry(depoproc[i][1],histName[i][1],"f") ;
 
 	return 0 ;
 }
